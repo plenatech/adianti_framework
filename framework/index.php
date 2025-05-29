a4b965919a9af0e02e4adc2cf090a4accfb545f0
Marco Driemeyer <marco.driemeyer@plenatech.com>
2025-05-29 11:11:42 -0300

    8.0.0

--- framework/index.php
+++ framework/index.php
@@ -1,17 +1,24 @@
 <?php
 require_once 'init.php';
+
+$ini = AdiantiApplicationConfig::get();
 $theme  = $ini['general']['theme'];
 new TSession;
 
 $content     = file_get_contents("app/templates/{$theme}/layout.html");
 $menu_string = AdiantiMenuBuilder::parse('menu.xml', $theme);
-$content     = str_replace('{MENU}', $menu_string, $content);
 $content     = ApplicationTranslator::translateTemplate($content);
 $content     = str_replace('{LIBRARIES}', file_get_contents("app/templates/{$theme}/libraries.html"), $content);
 $content     = str_replace('{class}', isset($_REQUEST['class']) ? $_REQUEST['class'] : '', $content);
 $content     = str_replace('{template}', $theme, $content);
 $content     = str_replace('{MENU}', $menu_string, $content);
+$content     = str_replace('{MENUTOP}', AdiantiMenuBuilder::parseNavBar('menu-top-public.xml', $theme), $content);
+$content     = str_replace('{MENUBOTTOM}', AdiantiMenuBuilder::parseNavBar('menu-bottom-public.xml', $theme), $content);
 $content     = str_replace('{lang}', $ini['general']['language'], $content);
+$content     = str_replace('{title}', $ini['general']['title'] ?? '', $content);
+$content     = str_replace('{template_options}',  json_encode($ini['template'] ?? []), $content);
+$content     = str_replace('{adianti_options}',  json_encode($ini['general']), $content);
+
 $css         = TPage::getLoadedCSS();
 $js          = TPage::getLoadedJS();
 $content     = str_replace('{HEAD}', $css.$js, $content);
